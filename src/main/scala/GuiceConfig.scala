5ac3c0df6a4ec76e4bc2acf2c4a3ccdbfe906d30
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-02-22 22:18:41 +0200

    Configure the production runtime with Guice

--- src/main/scala/GuiceConfig.scala
+++ src/main/scala/GuiceConfig.scala
@@ -1,9 +1,15 @@
-import com.google.inject.servlet.ServletModule
+import com.google.inject.Guice
+import com.google.inject.servlet.{GuiceServletContextListener, ServletModule}
+import GuiceConfig._
+
+class GuiceConfig extends GuiceServletContextListener {
+  def getInjector = Guice.createInjector(servletModule)
+}
 
 object GuiceConfig {
   def servletModule = new ServletModule() {
     override def configureServlets() {
-      val objectToGreet = System.getProperty("hello.target")
+      val objectToGreet = System.getProperty("hello.target", System.getProperty("user.name"))
       serve("/*").`with`(new HelloServlet(objectToGreet))
     }
   }
--- src/main/scala/ScalatraBootstrap.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatra._
-import javax.servlet.ServletContext
-
-class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    val personToGreet = System.getProperty("user.name")
-    context.mount(new HelloServlet(personToGreet), "/*")
-  }
-}
